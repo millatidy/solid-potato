ce55875c5ceff253c580ea3b6c6762ed06c9bde5
millatidy <tidymilla@gmail.com>
2019-03-24 16:24:47 +0200

    Inital project files add

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+from flask_cors import CORS
+# from flask_mail import Mail
+from raven.contrib.flask import Sentry
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+cors = CORS(app, resources={r"/*": {
+									"origins": [
+										"http://localhost:8080"
+										]
+									}
+								}
+							)
+# mail = Mail(app)
+
+if not app.debug:
+	# Sentry
+	sentry = Sentry(app, dsn='https://f70b58dfecda4089b51567122c36a881:4fcb5bf1b52f4ddc974db998dc3f1fd2@sentry.io/1292364')
+
+from app import routes, models
---
+++ quiz.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+
+Key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
+
+# Oh no! The code is going over the edge! What are you going to do?
+message = b'gAAAAABcl4kVUw8WLI-LJo1J2zs-DyLIIpL9_owX5E11LmOHPEgRDRJYRjFeqLgNXRCgfELU5IGtnlq0TPH44cV3z2_1Yp7EVwwsBnlcY1pM0SQ3ndhmFHLMGA9C_mmW_jdt-09slpxo_VRpkm0sSWDo4x9dtAjqthFwmx6gzcAQ_eaHA5F98MWi9lzKncPastpvqUGkiEhA'
+
+def main():
+    f = Fernet(Key)
+    print(f.decrypt(message))
+
+
+if __name__ == "__main__":
+    main()
