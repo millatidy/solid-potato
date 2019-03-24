739c1b6aeaa42e3d38cab458ffdf4f804f84e1a2
millatidy <tidymilla@gmail.com>
2019-03-24 22:58:31 +0200

    Call it a day. Added clients get post initaal code

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,46 @@
 from app import app
-from flask import render_template
+from .models import *
+from flask import request, jsonify
 
-@app.route('/')
-def index():
-	return "hello world"
+
+@app.route('/clients', methods=['GET', 'POST'])
+def clients():
+    if request.method == 'GET':
+        clients = []
+        client = Client()
+        clients = client.find_all()
+        return jsonify(clients)
+    else:
+        data = request.get_json()
+        client = Client()
+        client.name = data['name']
+        client.save()
+        return jsonify({"id": client.id, "name": client.name})
+
+@app.route('/clients/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def client(id):
+    pass
+
+@app.route('/product-areas', methods=['GET', 'POST'])
+def product_areas():
+    pass
+
+@app.route('/product-areas/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def product_area(id):
+    pass
+
+@app.route('/features', methods=['GET', 'POST'])
+def features():
+    pass
+
+@app.route('/features/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def feature(id):
+    pass
+
+@app.route('/feature-requests', methods=['GET', 'POST'])
+def feature_requests():
+    pass
+
+@app.route('/feature-requests/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def feature_request(id):
+    pass
--- quiz.py
+++ quiz.py
@@ -3,6 +3,8 @@ from cryptography.fernet import Fernet
 Key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 
 # Oh no! The code is going over the edge! What are you going to do?
+# F12 and click element inspector
+# Copy and paste from source
 message = b'gAAAAABcl4kVUw8WLI-LJo1J2zs-DyLIIpL9_owX5E11LmOHPEgRDRJYRjFeqLgNXRCgfELU5IGtnlq0TPH44cV3z2_1Yp7EVwwsBnlcY1pM0SQ3ndhmFHLMGA9C_mmW_jdt-09slpxo_VRpkm0sSWDo4x9dtAjqthFwmx6gzcAQ_eaHA5F98MWi9lzKncPastpvqUGkiEhA'
 
 def main():
