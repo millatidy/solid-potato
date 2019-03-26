1eb7e39f9b482baaafcceb50d0f341bed1e430e6
millatidy <tidymilla@gmail.com>
2019-03-27 00:09:18 +0200

    client api

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from flask import abort
+from flask import abort, url_for
 from datetime import datetime, timedelta
 
 class DAO(object):
@@ -36,11 +36,45 @@ class DAO_UNIQUE_NAME(DAO):
             return False
         return True
 
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
 
-class Client(DAO_UNIQUE_NAME, db.Model):
+
+class Client(PaginatedAPIMixin, DAO_UNIQUE_NAME, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True, index=True)
     requests = db.relationship('FeatureRequest', lazy='dynamic', backref='client')
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'name': self.name,
+            'links': {
+                'self': url_for('client', id=self.id),
+                'requests': url_for('feature_requests', client_id=1)
+            }
+        }
+        return data
 
 
 class ProductArea(DAO_UNIQUE_NAME, db.Model):
--- app/routes.py
+++ app/routes.py
@@ -6,20 +6,37 @@ from flask import request, jsonify
 @app.route('/clients', methods=['GET', 'POST'])
 def clients():
     if request.method == 'GET':
-        clients = []
-        client = Client()
-        clients = client.find_all()
-        return jsonify(clients)
+        page = request.args.get('page', 1, type=int)
+        per_page = min(request.args.get('per_page', 10, type=int), 100)
+        data = Client.to_collection_dict(Client.query, page, per_page, 'clients')
+        return jsonify(data)
     else:
         data = request.get_json()
         client = Client()
         client.name = data['name']
         client.save()
-        return jsonify({"id": client.id, "name": client.name})
+        return jsonify(client.to_dict())
 
 @app.route('/clients/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def client(id):
-    pass
+    if request.method == 'GET':
+        client = Client()
+        client = client.find_one(id)
+        return jsonify(client.to_dict())
+    elif request.method == 'PUT':
+        data = request.get_json()
+        client = Client()
+        client.id = data['id']
+        client.name = data['name']
+        client = client.update()
+        return jsonify(client.to_dict())
+    else:
+        data = request.get_json()
+        client = Client()
+        client.id = data['id']
+        client.delete()
+        return jsonify({'message': 'client deleted'})
+
 
 @app.route('/product-areas', methods=['GET', 'POST'])
 def product_areas():
@@ -34,7 +51,7 @@ def features():
     pass
 
 @app.route('/features/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-def feature(id):
+def get_requests(id):
     pass
 
 @app.route('/feature-requests', methods=['GET', 'POST'])
